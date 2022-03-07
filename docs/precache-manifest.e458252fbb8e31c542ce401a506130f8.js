d749162231d35dd047cbdf501ee3fc50c2a62384
Bruno Palermo <bruno.palermomesa023@gmail.com>
2022-03-07 15:04:07 -0300

    Docs change

---
+++ docs/precache-manifest.e458252fbb8e31c542ce401a506130f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5137b3934b5512fee4e82a2c98cf3328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfcecf5b47ece58fb55a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcecf5b47ece58fb55a",
+    "url": "/static/js/main.a169b034.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
